2387bd2918922d0edd06a801bdb5c7faf3bcd228
Zachary Cobb <zac.cobb9224@gmail.com>
2019-01-24 20:36:14 -0600

    Finished tweetparser

--- tweetparser.js
+++ tweetparser.js
@@ -1,18 +1,16 @@
 fs = require('fs');
 
-fs.readFile(`data.json`, `utf8`, function (err, data) {
+fs.readFile(`tweet.json`, `utf8`, function (err, data) {
     if (err) {
         return console.log(err);
     }
-    var dt = JSON.parse(data);
+    var parsedData = JSON.parse(data);
 
-    //console.log(Object.keys(dt.menu).length);
-    //console.log(dt.menu.header);
-
-    for(x in dt.menu.items){
-        if(dt.menu.items[x] != null){
-            var out = dt.menu.items[x].id;
-            console.log(out);
-        }
+    console.log(`Date: ${parsedData.created_at}`);
+    console.log(`Name: ${parsedData.user.name}`);
+    console.log(`Text: ${parsedData.text}`);
+    console.log(`Screen Name: ${parsedData.user.screen_name}`);
+    for(x in parsedData.entities.hashtags){
+        console.log(`Hashtag ${x}: ${parsedData.entities.hashtags[x].text}`);
     }
 });
